8e502550fa9b7d581d8b50f9db6496e20d233137
gcifre <gcifre@anysolution.org>
2024-09-10 13:45:43 +0200

    Added: api helpers with apiFetch function

---
+++ app/apidata/helpers.ts
@@ -0,0 +1,18 @@
+import { InvalidURL, ConnectionError } from "@/lib/errors-helpers";
+
+/**
+ * Fetches API contents and converts to JSON data
+ * @param url URL to set
+ * @returns JSON API data
+ */
+export async function apiFetch(url: string) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new ConnectionError("Error to connect to the API");
+    }
+    return await res.json();
+  } catch (error) {
+    throw new InvalidURL(`The URL is not valid ${url}`);
+  }
+}
