a366c20b180ee78fba4ba9658d6279e09ed18b31
gcifre <gcifre@anysolution.org>
2024-09-10 13:56:25 +0200

    Added: text area content component

--- app/apidata/page.tsx
+++ app/apidata/page.tsx
@@ -1,10 +1,12 @@
-import { InputForm } from "@/components/input-form/input-form";
+import { InputForm } from "@/components/forms/input-form";
+import { TextAreaContent } from "@/components/textarea-content/textarea-content";
 import React from "react";
 
 export default function Page() {
   return (
     <div>
       <InputForm />
+      <TextAreaContent title="Api content" data="test" type="api" />
     </div>
   );
 }
