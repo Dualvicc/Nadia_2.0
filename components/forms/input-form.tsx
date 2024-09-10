21ce37bf00f347ced69219f880e615b10e7c1a4c
gcifre <gcifre@anysolution.org>
2024-09-10 13:48:09 +0200

    Added: input form component

---
+++ components/forms/input-form.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const FormSchema = z.object({
+  url: z.string().url(),
+});
+
+export function InputForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      url: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {}
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-1/3 space-y-6 mb-8"
+      >
+        <FormField
+          control={form.control}
+          name="url"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-semibold text-lg">
+                Upload data
+              </FormLabel>
+              <FormControl>
+                <div className="flex w-full max-w-sm items-center space-x-2">
+                  <Input
+                    placeholder="https://www.api.com/api"
+                    {...field}
+                    className="bg-white"
+                  />
+                  <Button type="submit">Submit</Button>
+                </div>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
