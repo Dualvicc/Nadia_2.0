12feb3b323a911f4b90823381360c3c6626c4f4a
gcifre <gcifre@anysolution.org>
2024-09-16 13:44:25 +0200

    Modified: scafold

--- app/apidata/page.tsx
+++ app/apidata/page.tsx
@@ -1,3 +1,4 @@
+import { DataExtractForm } from "@/components/forms/data-extract-form";
 import { InputForm } from "@/components/forms/input-form";
 import { TextAreaContent } from "@/components/textarea-content/textarea-content";
 import React from "react";
@@ -6,7 +7,22 @@ export default function Page() {
   return (
     <div>
       <InputForm />
-      <TextAreaContent title="Api content" data="test" type="api" />
+      <TextAreaContent
+        title="Api content"
+        placeholder="Api data..."
+        data="test"
+        type="api"
+      />
+      <div>
+        <p>Select data to transform ngsi-ld</p>
+        <DataExtractForm data="hello" />
+      </div>
+      <TextAreaContent
+        title="Ngsi-ld content"
+        placeholder="Ngsi-ld"
+        data="test"
+        type="ngsild"
+      />
     </div>
   );
 }
