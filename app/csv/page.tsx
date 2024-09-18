8d12c2bb5c5ac0308a292a2b1e813b0d6d9f364d
gcifre <gcifre@anysolution.org>
2024-09-18 15:42:18 +0200

    Added: scaffold csv page

--- app/csv/page.tsx
+++ app/csv/page.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+"use client";
+
+import { SendData } from "@/components/buttons/send-data";
+import { DataExtractForm } from "@/components/forms/data-extract-form";
+import { InputForm } from "@/components/forms/input-form";
+import { TextAreaContent } from "@/components/textarea-content/textarea-content";
+import React, { useState } from "react";
 
 export default function Page() {
+  const [csvData, setCsvData] = useState("");
+  const [ngsildData, setNgsildData] = useState("");
+
   return (
     <div>
-      <h2>Csv page</h2>
+      <InputForm setData={setCsvData} />
+      <TextAreaContent
+        title="CSV content"
+        placeholder="Api data..."
+        data={csvData}
+        type="api"
+      />
+      <div>
+        <p className="font-semibold text-lg">
+          Select data to transform ngsi-ld
+        </p>
+        <DataExtractForm apiData={csvData} setApiData={setNgsildData} />
+      </div>
+      <TextAreaContent
+        title="Ngsi-ld content"
+        placeholder="Ngsi-ld..."
+        data={ngsildData}
+        type="ngsild"
+      />
+      <SendData ngsildData={ngsildData} />
     </div>
   );
 }
