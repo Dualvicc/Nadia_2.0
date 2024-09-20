dfc2e1cab86865011508aa3403e635ef0a9e2365
gcifre <gcifre@anysolution.org>
2024-09-20 10:49:59 +0200

    Modified: layout content

--- app/csv/page.tsx
+++ app/csv/page.tsx
@@ -7,26 +7,23 @@ import { InputForm } from "@/components/forms/input-form";
 import { TextAreaContent } from "@/components/textarea-content/textarea-content";
 import React, { useState } from "react";
 import { fetchCSVToJSON } from "./helpers";
+import { CSVTable } from "@/components/tables/csv-table";
 
 export default function Page() {
-  const [csvData, setCsvData] = useState("");
+  const [jsonData, setJsonData] = useState("");
   const [ngsildData, setNgsildData] = useState("");
+  const [csvData, setCsvData] = useState("");
 
   return (
     <div>
-      <InputForm fetch={fetchCSVToJSON} setData={setCsvData} />
-      <InputFileCSV setData={setCsvData} />
-      <TextAreaContent
-        title="CSV content"
-        placeholder="Api data..."
-        data={csvData}
-        type="api"
-      />
+      <InputForm fetch={fetchCSVToJSON} setData={setJsonData} />
+      <InputFileCSV setCsvData={setCsvData} setJsonData={setJsonData} />
+      <CSVTable title="CSV content" data={csvData} />
       <div>
         <p className="font-semibold text-lg">
           Select data to transform ngsi-ld
         </p>
-        <DataExtractForm apiData={csvData} setApiData={setNgsildData} />
+        <DataExtractForm apiData={jsonData} setApiData={setNgsildData} />
       </div>
       <TextAreaContent
         title="Ngsi-ld content"
