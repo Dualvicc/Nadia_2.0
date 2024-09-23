6a4f746faee742c24f4eb79d02bfa0271f279d50
vjaume <vjaume@anysolution.eu>
2024-09-23 18:47:34 +0200

    Fixed: bad import on encription and bad assignation to session variables

--- lib/client/utils.ts
+++ lib/client/utils.ts
@@ -1,65 +1,15 @@
-<<<<<<< HEAD
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { RectangleGroupIcon, TagIcon } from "@heroicons/react/24/solid";
-import { WebhookURLSchema } from "@/lib/client/types";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-
-export function validateInputText(value: string) {
-  return WebhookURLSchema.safeParse(value);
-}
-
-export function isStringBlank(str: string): boolean {
-  return str.trim().length === 0;
-}
-
-export function searchDataEntities(str: string, arr: Array<any>) {
-  return arr.filter(
-    (el) =>
-      el.id.toLowerCase().includes(str.toLowerCase()) ||
-      el.type.toLowerCase().includes(str.toLowerCase())
-  );
-}
-
-export function searchDataSubscriptions(str: string, arr: Array<any>) {
-  return arr.filter(
-    (el) =>
-      el.id.toLowerCase().includes(str.toLowerCase()) ||
-      el.description.toLowerCase().includes(str.toLowerCase()) ||
-      el.notification.http.url
-        .toLowerCase()
-        .includes(str.toLocaleLowerCase()) ||
-      el.notification.lastNotification
-        .toLocaleLowerCase()
-        .includes(str.toLocaleLowerCase())
-  );
-}
-
-export const links = [
-  { name: "Entities", href: "/", icon: RectangleGroupIcon },
-  {
-    name: "Subscriptions",
-    href: "/subscriptions",
-    icon: TagIcon,
-  },
-];
-=======
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { z } from "zod";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { decrypt } from "./encryption";
+import { decrypt } from "@/lib/encryption";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 export async function getIdToken() {
-  const session = await getServerSession(authOptions);
+  const session : any = await getServerSession(authOptions);
   if (session) {
     const idTokenDecrypted = decrypt(session.id_token);
     return idTokenDecrypted;
@@ -68,7 +18,7 @@ export async function getIdToken() {
 }
 
 export async function getAccessToken() {
-  const session = await getServerSession(authOptions);
+  const session : any = await getServerSession(authOptions);
   if (session) {
     const accessTokenDecrypted = decrypt(session.access_token);
     return accessTokenDecrypted;
@@ -88,4 +38,3 @@ export const ngsiLdSchema = z
     type: z.string(),
   })
   .catchall(attributeSchema.optional());
->>>>>>> main
