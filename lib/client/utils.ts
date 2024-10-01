91146a696ea751e159b276e27490ad559205d64b
Dualvicc <vmpporci@gmail.com>
2024-10-01 11:52:48 +0200

    Fixed server errors from server functions in client imports

--- lib/client/utils.ts
+++ lib/client/utils.ts
@@ -1,40 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { z } from "zod";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { decrypt } from "./encryption";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-export async function getIdToken() {
-  const session = await getServerSession(authOptions);
-  if (session) {
-    const idTokenDecrypted = decrypt(session.id_token);
-    return idTokenDecrypted;
-  }
-  return null;
-}
-
-export async function getAccessToken() {
-  const session = await getServerSession(authOptions);
-  if (session) {
-    const accessTokenDecrypted = decrypt(session.access_token);
-    return accessTokenDecrypted;
-  }
-  return null;
-}
-
-const attributeSchema = z.object({
-  type: z.string(),
-  value: z.unknown(),
-  metadata: z.record(z.unknown()).optional(),
-});
-
-export const ngsiLdSchema = z
-  .object({
-    id: z.string().regex(/^urn:ngsi-ld:/),
-    type: z.string(),
-  })
-  .catchall(attributeSchema.optional());
