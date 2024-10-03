8cecbf05bd2b8480c2af97f62808b89a1beaaa79
Dualvicc <vmpporci@gmail.com>
2024-10-03 11:23:57 +0200

    Added styles

--- components/auth/user.tsx
+++ components/auth/user.tsx
@@ -10,8 +10,8 @@ export const User = () => {
 
   if (!user) {
     return (
-      <div className="flex items-center w-full h-16 mb-4 border border-gray-100 rounded-full p-2">
-        <Skeleton className="h-12 w-12 rounded-full shrink-0" />
+      <div className="flex justify-center items-center w-[95%] h-12 mb-4 border border-gray-100 rounded-full p-2">
+        <Skeleton className="h-8 w-8 rounded-full shrink-0" />
         <div className="flex flex-col justify-center w-full ml-2">
           <Skeleton className="h-3 w-1/2 mb-1" />
           <Skeleton className="h-3 w-3/4" />
@@ -21,8 +21,8 @@ export const User = () => {
   }
 
   return (
-    <div className="flex items-center w-full h-16 mb-4 border border-gray-100 rounded-full p-2">
-      <Avatar className="h-12 w-12 shrink-0">
+    <div className="flex justify-center items-center w-[95%] h-12 mb-4 border border-gray-100 rounded-full p-2">
+      <Avatar className="h-8 w-8 shrink-0">
         <AvatarImage src={user.image ?? ""} alt="User Avatar" />
         <AvatarFallback>
           {user.name?.[0] ?? user.email?.[0] ?? "U"}
