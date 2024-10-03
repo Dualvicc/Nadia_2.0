650b5f590a2701facec5d95a2b1e375e4e721597
vjaume <vjaume@anysolution.eu>
2024-10-03 18:34:51 +0200

    Fixed: Add forgotten logic functions for entities and subscriptions

--- lib/client/utils.ts
+++ lib/client/utils.ts
@@ -1,6 +1,37 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { WebhookURLSchema } from "@/lib/client/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function validateInputText(value: string) {
+  return WebhookURLSchema.safeParse(value);
+}
+
+export function isStringBlank(str: string): boolean {
+  return str.trim().length === 0;
+}
+
+export function searchDataEntities(str: string, arr: Array<any>) {
+  return arr.filter(
+    (el) =>
+      el.id.toLowerCase().includes(str.toLowerCase()) ||
+      el.type.toLowerCase().includes(str.toLowerCase())
+  );
+}
+
+export function searchDataSubscriptions(str: string, arr: Array<any>) {
+  return arr.filter(
+    (el) =>
+      el.id.toLowerCase().includes(str.toLowerCase()) ||
+      el.description.toLowerCase().includes(str.toLowerCase()) ||
+      el.notification.http.url
+        .toLowerCase()
+        .includes(str.toLocaleLowerCase()) ||
+      el.notification.lastNotification
+        .toLocaleLowerCase()
+        .includes(str.toLocaleLowerCase())
+  );
 }
