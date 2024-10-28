2af746def594e62732b63f4c63def138a6dda716
vjaume <vjaume@anysolution.eu>
2024-10-28 22:41:34 +0100

    Added: dateTimeText to display datetime and sanitizeValue to replace the string with simple and double quotes

--- lib/client/utils.ts
+++ lib/client/utils.ts
@@ -10,6 +10,27 @@ export function validateInputText(value: string) {
   return WebhookURLSchema.safeParse(value);
 }
 
+export function dataTimeText() {
+  const dateVar = new Date();
+  const dateFormatted = dateVar.toLocaleDateString("en-EN", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const hourFormatted = dateVar.toLocaleTimeString("en-EN", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  return `${dateFormatted} ${hourFormatted}`;
+}
+
+export function sanitizeValue(value: string) {
+  return value.replace(/"/g, "_").replace(/'/g, "_");
+}
+
 export function isStringBlank(str: string): boolean {
   return str.trim().length === 0;
 }
