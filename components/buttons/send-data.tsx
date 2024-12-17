c19d3714442a2f57aa0e5184a6ec3c4c80bfcf4a
DiegoSehwerert <dsehwerert@gmail.com>
2024-12-18 00:24:35 +0100

    Refactor SendData component: standardize string quotes and improve error handling message.

--- components/buttons/send-data.tsx
+++ components/buttons/send-data.tsx
@@ -1,18 +1,17 @@
-"use client";
+'use client';
 
-import React from "react";
-import { Button } from "@/components/ui/button";
-import { isJSON, sendNGSIJson } from "@/lib/client/helpers";
-import { InvalidData } from "@/lib/errors";
-import { toast } from "@/hooks/use-toast";
-import { dataTimeText } from "@/lib/client/utils";
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { isJSON, sendNGSIJson } from '@/lib/client/helpers';
+import { InvalidData } from '@/lib/errors';
+import { toast } from '@/hooks/use-toast';
+import { dataTimeText } from '@/lib/client/utils';
 
 export function SendData({ ngsildData }: { ngsildData: string }) {
-
   async function sendDataToApi() {
     try {
       if (!ngsildData || !isJSON(ngsildData)) {
-        throw new InvalidData("Invalid Data");
+        throw new InvalidData('Invalid Data');
       }
       const jsonData: any = JSON.parse(ngsildData);
       const res = await sendNGSIJson(jsonData);
@@ -22,7 +21,7 @@ export function SendData({ ngsildData }: { ngsildData: string }) {
       }
 
       toast({
-        title: "Data sent succesfully",
+        title: 'Data sent succesfully',
         description: dataTimeText(),
       });
     } catch (e) {
